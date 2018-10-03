f00e838e755a99c27745b7688499dcece10b0f37
Tony Fu <tonyfu97@email.arizona.edu>
2018-10-03 13:59:47 -0700

    Add files via upload
    added some features with javascript.

--- system.js
+++ system.js
@@ -2,6 +2,14 @@
    Tony Fu
    system.js
  */
+var coreUsage = [66, 47, 31, 85];
+var coreTemperature = [90, 81, 88, 105];
+
+var RAMUsage = [22, 46, 59];
+
+var driveUsage = [55, 89];
+var driveTemperature = [111, 73];
+
 (function() {
 	window.onload = function ()
 	{
@@ -12,10 +20,47 @@
 		CPUDrop.onclick = list;
 		RAMDrop.onclick = list;
 		driveDrop.onclick = list;
+		console.log(hello);
 	}
 	
 	function list() {
+		
+		document.getElementById("CPU").style.backgroundColor = "#303030";
+		document.getElementById("RAM").style.backgroundColor = "#303030";
+		document.getElementById("Drives").style.backgroundColor = "#303030";
+		
 		this.style.backgroundColor = "ForestGreen";
+		
+		data(this.innerHTML);
+	}
+	
+	function graph(type)
+	{
+		var length;
+		var dataArea = document.getElementById("dataArea");
+		dataArea.innerHTML = "";
+		if (type == "RAM")
+		{
+			length = RAMUsage.length;
+			var table = document.createElement("table");
+			
+			dataArea.appendChild(table);
+		}
+		if (type == "Drives")
+		{
+			length = driveUsage.length;
+			var table = document.createElement("table");
+			
+			dataArea.appendChild(table);
+		}
+		if (type == "CPU")
+		{
+			length = coreUsage.length;
+			var table = document.createElement("table");
+			
+			dataArea.appendChild(table);
+		}
+		
 	}
 	
 })();
