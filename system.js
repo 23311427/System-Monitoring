77d335cfaada494fbc768e34e1f3088a9123c5b2
Tony Fu <tonyfu97@email.arizona.edu>
2018-10-22 21:30:40 -0700

    Add files via upload
    added in the new look. side bar navigation now supports a set amount of cpus, drives, and cores.

--- system.js
+++ system.js
@@ -10,6 +10,10 @@ var RAMUsage = [22, 46, 59];
 var driveUsage = [55, 89];
 var driveTemperature = [111, 73];
 
+var ram = 0;
+var cpu = 0;
+var drive = 0;
+
 (function() {
 	window.onload = function ()
 	{
@@ -22,73 +26,219 @@ var driveTemperature = [111, 73];
 		driveDrop.onclick = list;
 	}
 	
+	function clickFunction()
+	{
+		var list = document.getElementsByClassName("expand");
+		for (var i = 0; i < list.length; i++)
+		{
+			list[i].style.backgroundColor = "#303030";
+		}
+		document.getElementById("CPU").style.backgroundColor = "#303030";
+		document.getElementById("RAM").style.backgroundColor = "#303030";
+		document.getElementById("Drives").style.backgroundColor = "#303030";
+		this.style.backgroundColor = "ForestGreen";
+	}
+	
+	// changes the background color of the sidebar navigation to green
 	function list() {
 		
 		document.getElementById("CPU").style.backgroundColor = "#303030";
 		document.getElementById("RAM").style.backgroundColor = "#303030";
 		document.getElementById("Drives").style.backgroundColor = "#303030";
-		
+		var list = document.getElementsByClassName("expand");
+		for (var i = 0; i < list.length; i++)
+		{
+			list[i].style.backgroundColor = "#303030";
+		}
 		this.style.backgroundColor = "ForestGreen";
 		
 		data(this.innerHTML);
 	}
 	
+	function graph(idType)
+	{
+		
+	}
+	
+	function makeTable(length, type)
+	{
+		var table = document.createElement("table");
+		var th = document.createElement("tr");
+		if (type == "RAM")
+		{
+			th.innerHTML = "<th>"+type+"</th>" + "<th>Usage %</th>";
+		}
+		else 
+		{
+			th.innerHTML = "<th>"+type+"</th>" + "<th>Usage %</th>" + "<th>Temperature</th>";
+		}
+		table.appendChild(th);
+		
+		for (var i = 0; i < length; i++)
+		{
+			var tr = document.createElement("tr");
+			tr.innerHTML = "<td>"+i+"</td>";
+			if (type == "RAM")
+			{
+				tr.innerHTML += "<td>"+RAMUsage[i]+"</td>";
+			}
+			if (type == "Drives")
+			{
+				tr.innerHTML += "<td>"+driveUsage[i]+"</td>";
+				tr.innerHTML += "<td>"+driveTemperature[i]+"</td>";
+			}
+			if (type == "CPU")
+			{
+				tr.innerHTML += "<td>"+coreUsage[i]+"</td>";
+				tr.innerHTML += "<td>"+coreTemperature[i]+"</td>";
+			}
+			table.appendChild(tr);
+		}
+		document.getElementById("dataArea").appendChild(table);
+	}
+	
+	// displays the data in a table depending on which value is clicked
 	function data(type)
 	{
 		var length;
 		var dataArea = document.getElementById("dataArea");
 		dataArea.innerHTML = "";
+		var sidebar = document.getElementById("sidebar");
+		
 		if (type == "RAM")
 		{
 			length = RAMUsage.length;
-			
-			var table = document.createElement("table");
-			var th = document.createElement("tr");
-			th.innerHTML = "<th>"+type+"</th>" + "<th>Usage %</th>";
-			table.append(th);
-			for (var i = 0; i < length; i++)
+			if (ram == 0)
 			{
-				var tr = document.createElement("tr");
-				tr.innerHTML = "<td>"+i+"</td>";
-				tr.innerHTML += "<td>"+RAMUsage[i]+"</td>";
-				/*tr.innerHTML += "<td>"+coreTemperature[i]+"</td>";*/
-				table.append(tr);
+				ram = 1;
+				
+				// expand the sidebar when a choice is made
+				for (var i = 0; i < length; i++)
+				{
+					var div = document.createElement("div");
+					div.innerHTML = type + " " + (i+1);
+					div.id = type + "" + (i+1);
+					div.className = "expand";
+					div.onclick = clickFunction;
+					sidebar.insertBefore(div, document.getElementById("Drives"));
+				}
+				// table logic
+				makeTable(length, type);
 			}
-			dataArea.appendChild(table);
+			else 
+			{
+				ram++;
+				if (ram % 2 == 1)
+				{
+					for (var i = 0; i < length; i++)
+					{
+						var idName = type + "" + (i+1);
+						var div = document.getElementById(idName);
+						div.style.display = "none";
+					}
+				}
+				else 
+				{
+					for (var i = 0; i < length; i++)
+					{
+						var idName = type + "" + (i+1);
+						var div = document.getElementById(idName);
+						div.style.display = "block";
+					}
+				}
+				
+				makeTable(length, type);
+			}
 		}
 		if (type == "Drives")
 		{
 			length = driveUsage.length;
-			var table = document.createElement("table");
-			var th = document.createElement("tr");
-			th.innerHTML = "<th>"+type+"</th>" + "<th>Usage %</th>" + "<th>Temperature</th>";
-			table.append(th);
-			for (var i = 0; i < length; i++)
+			if (drive == 0)
 			{
-				var tr = document.createElement("tr");
-				tr.innerHTML = "<td>"+i+"</td>";
-				tr.innerHTML += "<td>"+driveUsage[i]+"</td>";
-				tr.innerHTML += "<td>"+driveTemperature[i]+"</td>";
-				table.append(tr);
+				drive = 1;
+				// expand the sidebar when a choice is made
+				for (var i = 0; i < length; i++)
+				{
+					var div = document.createElement("div");
+					div.innerHTML = type + " " + (i+1);
+					div.id = type + "" + (i+1);
+					div.className = "expand";
+					div.onclick = clickFunction;
+					sidebar.insertBefore(div, document.getElementById("CPU"));
+				}
+				// table logic
+				makeTable(length, type);
 			}
-			dataArea.appendChild(table);
+			else 
+			{
+				drive++;
+				if (drive % 2 == 1)
+				{
+					for (var i = 0; i < length; i++)
+					{
+						var idName = type + "" + (i+1);
+						var div = document.getElementById(idName);
+						div.style.display = "none";
+					}
+				}
+				else 
+				{
+					for (var i = 0; i < length; i++)
+					{
+						var idName = type + "" + (i+1);
+						var div = document.getElementById(idName);
+						div.style.display = "block";
+					}
+				}
+				
+				makeTable(length, type);
+			}
+			
 		}
 		if (type == "CPU")
 		{
 			length = coreUsage.length;
-			var table = document.createElement("table");
-			var th = document.createElement("tr");
-			th.innerHTML = "<th>"+type+"</th>" + "<th>Usage %</th>" + "<th>Temperature</th>";
-			table.append(th);
-			for (var i = 0; i < length; i++)
+			if (cpu == 0)
 			{
-				var tr = document.createElement("tr");
-				tr.innerHTML = "<td>"+i+"</td>";
-				tr.innerHTML += "<td>"+coreUsage[i]+"</td>";
-				tr.innerHTML += "<td>"+coreTemperature[i]+"</td>";
-				table.append(tr);
+				cpu = 1;
+				
+				// expand the sidebar when a choice is made
+				for (var i = 0; i < length; i++)
+				{
+					var div = document.createElement("div");
+					div.innerHTML = type + " " + (i+1);
+					div.id = type + "" + (i+1);
+					div.className = "expand";
+					div.onclick = clickFunction;
+					sidebar.appendChild(div);
+				}
+				// table logic
+				makeTable(length, type);
 			}
-			dataArea.appendChild(table);
+			else 
+			{
+				cpu++;
+				if (cpu % 2 == 1)
+				{
+					for (var i = 0; i < length; i++)
+					{
+						var idName = type + "" + (i+1);
+						var div = document.getElementById(idName);
+						div.style.display = "none";
+					}
+				}
+				else 
+				{
+					for (var i = 0; i < length; i++)
+					{
+						var idName = type + "" + (i+1);
+						var div = document.getElementById(idName);
+						div.style.display = "block";
+					}
+				}
+				
+				makeTable(length, type);
+			}
 		}
 		
 	}
